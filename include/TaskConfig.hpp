cf761686ea641a1c18be20f34317bfccd4297edf
Peter Sammer <peter.sammer@v2c2.at>
2023-03-12 15:41:28 +0100

    try to make OTA more reliable?!?!

--- include/TaskConfig.hpp
+++ include/TaskConfig.hpp
@@ -7,7 +7,7 @@ public:
   static constexpr uint32_t WiFi_http_priority = 3;
 
   static constexpr uint32_t WiFi_ota_stacksize = 5000u;  // words
-  static constexpr uint32_t WiFi_ota_priority = 4;
+  static constexpr uint32_t WiFi_ota_priority = 1;
 
   static constexpr uint32_t Alarm_stacksize = 5000u;  // words
   static constexpr uint32_t Alarm_priority = 2;
